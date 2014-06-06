b5a8c26d3f65e4a237a1103eeb60625fc9482e14
Stef Sy <steffisy@gmail.com>
2014-06-06 18:42:09 +0800

    used nltk to tokenize. arbitrary # of arg inputs ok

--- mkv.py
+++ mkv.py
@@ -6,15 +6,20 @@
 
 from __future__ import division
 from collections import defaultdict
+from nltk.collocations import *
+
+import sys
+import nltk
 import timeit
 import random
-
 
 def createDict(fpath):
 	f = open(fpath,'r')
 	text = []
 	for line in f: 
-		text.extend( line.rstrip("\n").rstrip("\r").strip('\"').split(" ") )
+		# text.extend( line.rstrip("\n").rstrip("\r").strip('\"').split(" ") )
+		# nltk for the save
+		text.extend(nltk.tokenize.wordpunct_tokenize(line))
  	text = filter(None,text) #remove blanks
 
 	d = defaultdict(list)
@@ -28,33 +33,38 @@ def createDict(fpath):
 			pass
 	return d
 
-def mergeTexts(d1,d2):
-	mergedDict = d1
-	for k, v in d2:
-		mergedDict[k].append(v)
-	return mergedDict
+def mergeTexts(listLookups):
+	mergedLookup = defaultdict(list)
+	for lookup in listLookups:
+		for k, v in lookup.items():
+			mergedLookup[k].extend(v)
+	return mergedLookup
 
-#test function, flesh out laster
+# flesh out later
 def checkDict(d):
-	for k in d:
+	kError = 0
+	k = []
+	for k in d.keys():
 		if len(k) != 2:
-			print k
-			break
+			kError+=1
+			k.append(k)
+	print kError
 
 def writeNew(chain,nWords):
 	#pick the most common key as the first two words
-	key = random.choice(chain.keys())
-	val = chain[key][0]
+	key = max(chain.iterkeys(), key=(lambda key: len(chain[key])))
+	val = random.choice(chain[key])
 	wc = 3
 	newText = key[0] + " " + key[1] + " " + val
 
 	#keep adding new words, if key not found, select random word
-	#could be cleaned up, type checking seems like it can be removed
+	#could be cleaned up, type checking feels awkward
 	while wc < nWords:
 		key = (key[1],val)
 		if key in chain: 	
 			val = random.choice(chain[key]) 
 		else: 
+			# if only one possible value, output is string, else is a list of strs
 			r = random.choice(chain.values())
 			if type(r) is str:
 				val = r
@@ -64,33 +74,33 @@ def writeNew(chain,nWords):
 		wc+=1 
 	return newText
 
-
 if __name__ == '__main__':
 
-	d1 = createDict('holmes.txt')
-	d2 = createDict('styles.txt')
-	# d3 = createDict('grimms.txt')
-	# d4 = createDict('holmesreturn.txt')
+	lookups = []
+	for i in range(1,len(sys.argv)-1):
+		lookups.append(createDict(sys.argv[i]))
 
-	s1 = set(d1.keys())
-	s2 = set(d2.keys())
-	# s3 = set(d3.keys())
-	# s4 = set(d4.keys())
-
-	# 1, the setup required to run timeit on custom defined funtions is stupid
-	# 2, createDict() takes about 2.2s to run on a 595kb text file
-	# print(timeit.timeit("createDict('holmes.txt')", setup="from __main__ import createDict",number=10))	
+	# 1, the setup required to run timeit on custom defined funtions is too much
+	# 2, createDict() takes about 2.1s to run on a 595kb text file
+	# print(timeit.timeit("createDict('holmes.txt')", setup="from __main__ import createDict",number=50))	
 	
+	#write 200 words of the new mashup
+	mashup = mergeTexts(lookups)
+	print writeNew(mashup,100)
+
 	# what % of all keys do the two texts have in common? 
 	# # the higher this is, the better the mashup's going to be
 
-	print "Holmes vs. Mystery at Styles", len(s1.intersection(s2)) / len(s1.union(s2))
+	# s1 = set(d1.keys())
+	# s2 = set(d2.keys())
+	# s3 = set(d3.keys())
+	# s4 = set(d4.keys())
+
+	# print "The Fairy Tales of Sherlock Holmes"
+	# print "Mashup Rating:", len(s1.intersection(s3)) / len(s1.union(s3))
 	# print "Holmes vs. Grimms' Tales" ,len(s1.intersection(s3))/ len(s1.union(s3))
 	# print "Mystery at Styles vs. Grimms'" ,len(s2.intersection(s3))/ len(s2.union(s3))
 	# print "Holmes vs Holmes", len(s1.intersection(s4))/ len(s1.union(s4))
 
-	# checkDict(d1)
 
-	#write 200 words of the new mashup
-	mashup = mergeTexts(d1,d2)
-	print writeNew(mashup,200)
+	
